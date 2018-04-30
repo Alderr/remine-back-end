09f314406070224882e74cf67cf2991ffa9f3dc7
Vernon Mensah <vernonmensah@gmail.com>
2018-04-30 02:20:35 -0400

    added process.env.port

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const server = jsonServer.create()
 const router = jsonServer.router('final-test.json')
 const middlewares = jsonServer.defaults()
 
+const PORT = process.env.PORT || 3001;
+
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running');
 });
